658772c0fd71d0c1f26c34da37ed325c99b6cbfc
kay1403 <angekoumba754@gmail.com>
2025-05-17 18:35:26 +0200

    Create models.py

---
+++ alx_travel_app/listings/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    price_per_night = models.DecimalField(max_digits=8, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
